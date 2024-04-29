afb1ab6043709e472e995d5b136fe9648b597035
Ultra-pn4rv <tsarultra@gmail.com>
2024-04-29 11:14:57 -0400

    questao 2

---
+++ questao2.js
@@ -0,0 +1,9 @@
+/*
+Questão 02: Escreva uma arrow function chamada saudacao que recebe um nome 
+como parâmetro e retorna uma string de saudação. 
+*/
+
+function saudacao(nome) {
+    return ("Olá, " + nome + "!")
+}
+console.log(saudacao("Felipe"));
