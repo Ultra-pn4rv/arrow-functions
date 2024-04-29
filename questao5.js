37f9fb5656f1a8336a7f004a6e38c6b19b2d8cc5
Ultra-pn4rv <tsarultra@gmail.com>
2024-04-29 11:15:15 -0400

    questao 5

---
+++ questao5.js
@@ -0,0 +1,9 @@
+/*
+Questão 05: Escreva uma arrow function chamada maiorNumero que recebe dois 
+números como parâmetros e retorna o maior deles.
+*/
+
+const maiorNumero = (a, b) => {
+    return Math.max(a, b);
+};
+console.log(maiorNumero(7, 5))
