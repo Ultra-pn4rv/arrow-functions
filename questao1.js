32c85c5f923cd5959f2d66f4b41521d2468bbb11
Ultra-pn4rv <tsarultra@gmail.com>
2024-04-29 11:14:49 -0400

    questao 1

---
+++ questao1.js
@@ -0,0 +1,7 @@
+/*
+Questão 01: Escreva uma arrow function chamada dobro que recebe um número 
+como parâmetro e retorna o dobro desse número.
+*/
+
+const dobro = x => x * x;
+console.log(dobro(3))
