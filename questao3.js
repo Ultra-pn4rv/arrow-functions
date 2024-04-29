4990de8985557e034ba84f2be612113183b54762
Ultra-pn4rv <tsarultra@gmail.com>
2024-04-29 11:15:01 -0400

    questao 3

---
+++ questao3.js
@@ -0,0 +1,9 @@
+/*
+Questão 03: Escreva uma arrow function chamada somaArray que recebe um array 
+de números como parâmetro e retorna a soma de todos os elementos desse array. 
+*/
+
+function soma(a, b) {
+    return (a + b)
+}
+console.log(soma(3, 4));
